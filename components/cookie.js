7e60501a8abbff899b0a4878b1a8ce25bc5ab537
IktaNikuba <baukinv4@gmail.com>
2024-09-26 12:13:31 +0300

    Fixed duplicate

--- components/cookie.js
+++ components/cookie.js
@@ -1,5 +1,5 @@
 const cookieUtils = (() => {
-  function setCookie(name, value, days) {
+  const setCookie = (name, value, days) => {
     let expires = "";
     if (days) {
       let date = new Date();
@@ -7,9 +7,9 @@ const cookieUtils = (() => {
       expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  }
+  };
 
-  function getCookie(name) {
+  const getCookie = (name) => {
     let nameEQ = name + "=";
     let cookies = document.cookie.split(';');
 
@@ -20,37 +20,9 @@ const cookieUtils = (() => {
     }
 
     return null;
-  }
+  };
 
-  return {
-    setCookie,
-    getCookie
-  }
+  return { setCookie, getCookie };
 })();
-
-const cookieUtils = {
-  setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-      let date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  },
-
-  getCookie(name) {
-    let nameEQ = name + "=";
-    let cookies = document.cookie.split(';');
-
-    for (let i = 0; i < cookies.length; i++) {
-      let cookieItem = cookies[i];
-      while (cookieItem.charAt(0) === ' ') cookieItem = cookieItem.substring(1, cookieItem.length);
-      if (cookieItem.indexOf(nameEQ) === 0) return cookieItem.substring(nameEQ.length, cookieItem.length);
-    }
-
-    return null;
-  }
-};
 
 export default cookieUtils;
